ece3443b0d58063acd3f104e0f4d63358e1308ae
Eliot Ozaki <eliot.ozaki@smetters.net>
2020-07-18 22:58:18 +0000

    made SQL data server connection

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,20 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="eliotozaki",
+    password="pyth0n4nywh3r3",
+    hostname="zewumpire.mysql.pythonanywhere-services.com",
+    databasename="zewumpire$scores",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 comments = []
 @app.route("/", methods=["GET", "POST"])
 def index():
