3cc40cb56ac13554edb214a7c510f0508ea8b53d
Eliot Ozaki <eliot.ozaki@smetters.net>
2020-07-18 23:18:01 +0000

    defined sql table

--- flask_app.py
+++ flask_app.py
@@ -7,8 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["DEBUG"] = True
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="eliotozaki",
-    password="pyth0n4nywh3r3",
+    username="zewumpire",
+    password="MySQL2416",
     hostname="zewumpire.mysql.pythonanywhere-services.com",
     databasename="zewumpire$scores",
 )
@@ -16,6 +16,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 comments = []
 @app.route("/", methods=["GET", "POST"])
 def index():
